1d97c432afe97172fa8f085db592d1107306b245
Shenny John <shenny@debian.local>
2019-04-06 10:53:21 +0400

    My first

---
+++ VAluechange.py
@@ -0,0 +1,13 @@
+firstvariable = input('Enter the value for A : ')
+secondvariable = input('Enter the value for B : ')
+print("Value of first variable A  now is %s" %(firstvariable))
+print("Value of Second variable B now is %s" %(secondvariable))
+print('Now we are going to change the value of A to B and B to A using third vaiable')
+print('-------------------------------------------')
+C = firstvariable
+firstvariable = secondvariable
+secondvariable = C
+print("Now the changed values are  : " )
+print("Value of first variable A now is %s" %(firstvariable))
+print("Value of second variable B now is %s" %(secondvariable))
+
